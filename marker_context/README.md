f7cfc311fa8e84966afac3c77407685fd6b6bae2
Jonathan Manning <jmanning@ebi.ac.uk>
2021-02-01 16:51:12 +0000

    Add new plots and text

--- marker_context/README.md
+++ marker_context/README.md
@@ -6,16 +6,38 @@ To illustrate this point, we derived marker gene sets for cell types from the pa
 
 ## Results  
 
-![Differential gene sets between contexts](markers_context.png)
+## Intersect of marker gene sets between contexts across the adjusted p value range
 
-The above figure shows how the number of differential (marker) genes and intersecting proportions between contexts evolves as the p value threshold is relaxed. The key points to note are:
+![Differential gene sets between contexts across adjusted p value range](markers_context_pval.png)
+
+The above figure shows how the number of differential (marker) genes and intersecting proportions between contexts evolves as the p value threshold is relaxed. This is shown for two 'denominator' values- the union of the two gene sets, and the smaller of the two input contexts - in this case pancreas. 
+
+The key points to note are:
 
  - Some cell types have few differential genes even at high p value thresholds, e.g. acinar cells
  - The Proportion of intersect tends towards 1 as the p value threshold is relaxed
  - Many more marker genes result from a whole mouse context than the pancreas-specific context. This makes sense: we can expect that pancreas cell types are more similar to other pancreas cell types than they are to cells from other organ systems. 
  - Some cell types have non-trivial intersects across the p value range (e.g. type B pancreatic cell) implying there are context-independent marker genes, but since even at high stringency the overall intersect is low (< 0.2), this set could not be identified from either marker set alone.
+ - In many cases the most significant genes in the smaller context (pancreas in this example) are a subset of those in the wider context at the same p value. 
  - Some cell types only develop significant intersect at low stringency (e.g. pancreatic PP cell), indicating that the marker genes are more context specific. 
+ - The drop in the intersect_pancreas trace for e.g. A cells and D cells suggests that there are highly significant marker genes not shared between contexts for those cell types. 
 
-## Conclusion
+## Intersect of top-ranked gene sets
 
-It is likely possible to derive a set of context-independent marker genes, which mark a given cell type irrespective of the background against which a cell type is compared. Genes such as insulin expressed only in one or few cell types are easy to assign to such scenarios. However gene sets derived from different contexts are very different when derived in isolation, in a manner specific to the cell types involved, so when we use such marker gene sets we should take care to state clearly the context in which they were derived, and exercise caution when a) comparing with other marker gene sets and b) applying these gene sets in different contexts, for example in cell type classification.                                                                                                                                                                                                                                       
+As an alternative view, we can examine the intersect between the top N genes between contexts:
+
+![Differential gene sets between contexts across ranks](markers_context_rank.png)
+
+This plot shows the intersection between marker gene sets of the same rank (rank coeff = 1), and different ranks (rank coeff > 1), allowing for a more permissive comparison. For example, at a coefficient of 3 we're examining how many of e.g. the top 10 genes of the pancreas context are present in the top 30 genes of the whole mouse context. 
+
+Many cell types show high ( ~ 75\% or above) agreement between ranked marker gene sets over a wide range of ranks at relatively modest rank coefficients (see e.g. endothelial cells,  leukocytes, D cells, ductal cells, PP cells, and especially B cells), indicating robust marker gene sets. Some types (e.g. A cells, stellate cells) agree only at higher ranks, suggesting context specificity, in agreement with the plots in the section above. 
+
+
+## Conclusions
+
+ - Wider contexts produce longer differential gene lists.
+ - Marker genes derived from narrow contexts largely persist in the marker gene sets for wider contexts (but not vice versa), so it may be possible to infer 'upwards' applicability of marker gene sets. 
+ - Extent and character of agreement between marker contexts is specific to the cell types concerned. 
+
+In all cases care should be taken to state clearly the context in which marker gene sets are derived. 
+
--- marker_context/compare_difflevel_markers.R
+++ marker_context/compare_difflevel_markers.R
@@ -1,12 +1,26 @@
 #!/usr/bin/env Rscript
 
+library(ggplot2)
+
 # Take two marker gene lists derived from different contexts, and see how the
 # overlap in genes changes as we make p value more and more stringent.
 
-cl <- commmandArgs(trailingOnly = TRUE)
+cl <- commandArgs(trailingOnly = TRUE)
 
-marker_file_1 <- "~/output/E-ENAD-15.pancreas.markers.tsv"
-marker_file_2 <- "~/output/E-ENAD-15.markers.tsv"
+marker_file_1 <- cl[1]
+context_1 <- cl[2]
+marker_file_2 <- cl[3]
+context_2 <- cl[4]
+mode <- cl[5]
+outfile <- cl[6]
+
+#marker_file_1 <- "~/output/E-ENAD-15.pancreas.markers.tsv"
+#marker_file_2 <- "~/output/E-ENAD-15.markers.tsv"
+#context_1 <- 'pancreas'
+#context_2 <- 'whole mouse'
+#mode = 'pval'
+#rank_coeff <- 3
+#outfile <- '~/Desktop/marker_context_rank.png'
 
 markers1 <- read.delim(marker_file_1, stringsAsFactors = FALSE)
 markers2 <- read.delim(marker_file_2, stringsAsFactors = FALSE)
@@ -17,47 +31,96 @@ common_celltypes <- unique(intersect(markers1$cluster, markers2$cluster))
 # thrsholds, starting at the minimum adjusted p value for each cell type
 
 prop_overlaps <- do.call(rbind, lapply(common_celltypes, function(ct){
-  min_pval <- floor(log10(min(markers1$pvals_adj[markers1$cluster == ct & markers1$pvals_adj > 0])))
-  pval_limits <- 10^-c(0:abs(min_pval))
   
-  po <- do.call(rbind, lapply(pval_limits, function(pl){
-    sig_genes1 <- markers1$genes[markers1$cluster == ct & markers1$pvals_adj < pl ]
-    sig_genes2 <- markers2$genes[markers2$cluster == ct & markers2$pvals_adj < pl ]
+  print(ct)
+  
+  if (mode == 'pval'){
     
-    intersect <- length(intersect(sig_genes1, sig_genes2))
-    union <- length(union(sig_genes1, sig_genes2))
+    min_pval <- floor(log10(min(markers1$pvals_adj[markers1$cluster == ct & markers1$pvals_adj > 0])))
+    thresholds <- 10^-c(0:abs(min_pval))
     
-    if (intersect == 0 || union == 0){
-      0
-    }else{
-      intersect / union
-    }
-    data.frame(cell_type = ct, pval_limit = pl, prop_overlap = intersect/ union, context = c('pancreas', 'whole mouse'), differential_genes = c(length(sig_genes1), length(sig_genes2)))
-  }))
+    do.call(rbind, lapply(thresholds, function(t){
+      sig_genes1 <- markers1$genes[markers1$cluster == ct & markers1$pvals_adj < t ]
+      sig_genes2 <- markers2$genes[markers2$cluster == ct & markers2$pvals_adj < t ]
+      
+      intersect <- length(intersect(sig_genes1, sig_genes2))
+      union <- length(union(sig_genes1, sig_genes2))
+      
+      res <- data.frame(cell_type = ct, intersect_union = intersect/ union, intersect_denominator = intersect/ length(sig_genes1), context = c(context_1, context_2), differential_genes = c(length(sig_genes1), length(sig_genes2)))
+      res[[mode]] <- t
+      res
+    }))
+  }else{
+    
+    max_rank <- min(c(1000, min(max(markers1$rank[markers1$cluster == ct]), max(markers1$rank[markers1$cluster == ct]))))
+    thresholds <- 0:max_rank
+    
+    po <- do.call(rbind, lapply(thresholds, function(t){
+      sig_genes1 <- markers1$genes[markers1$cluster == ct & markers1$rank <  t ]
+      do.call(rbind, lapply(c(1, 3, 5, 10), function(rank_coeff){
+        sig_genes2 <- markers2$genes[markers2$cluster == ct & markers2$rank < min((t * rank_coeff), max_rank) ] 
+        
+        intersect <- length(intersect(sig_genes1, sig_genes2))
+        union <- length(union(sig_genes1, sig_genes2))
+        
+        res <- data.frame(cell_type = ct, intersect_union = intersect/ union, intersect_denominator = intersect/ length(sig_genes1), context = c(context_1, context_2), differential_genes = c(length(sig_genes1), length(sig_genes2)))
+        res$rank_coeff <- rank_coeff
+        res[[mode]] <- t
+        res
+      }))
+    }))
+    po$rank_coeff <- factor(po$rank_coeff)
+    po
+  }
 }))
+
+colnames(prop_overlaps)[colnames(prop_overlaps) == 'intersect_denominator'] <- paste0('intersect_', context_1)
 
 # Plot the progressive change in intersect proportion for cell types over the
 # list of p value thresholds. Plot the number differential genes on a second
 # axis to provide context.
 
-ggplot(prop_overlaps, aes(x = log10(pval_limit))) + 
-  geom_line(aes(y = prop_overlap)) +
-  geom_line(aes(y = log10(differential_genes)/5, color = context)) +
-  scale_y_continuous(
+if (mode == 'pval'){
+  
+  # Reshape to plot different intersects alongside
+  
+  plotdata <- reshape2::melt(prop_overlaps, id.var = c('cell_type', 'context', mode, 'differential_genes'))
+  colnames(plotdata)[5:6] <- c('overlap_type', 'prop_overlap')
+  
+  p <- ggplot(plotdata, aes(x = log10(pval))) +
+    geom_line(aes(y = prop_overlap, linetype = overlap_type)) +
+    geom_line(aes(y = log10(differential_genes)/5, color = context)) +
+    scale_y_continuous(
     
-    # Features of the first axis
-    name = "Proportion overlap significant marker genes (intersect/ union)",
+      # Features of the first axis
+      name = "Proportion overlap significant marker genes",
     
-    # Add a second axis and specify its features
-    sec.axis = sec_axis(~.*5, name="Log10(Number of marker genes)")
-  ) +
+      # Add a second axis and specify its features
+      sec.axis = sec_axis(~.*5, name="Log10(Number of marker genes)")
+    ) +
+    xlab("log10(Adjusted p value limit)")
+}else{
+  
+  # Reshape to plot different intersects alongside
+  
+  plotdata <- reshape2::melt(prop_overlaps, id.var = c('cell_type', 'context', mode, 'differential_genes', 'rank_coeff'))
+  colnames(plotdata)[6:7] <- c('overlap_type', 'prop_overlap')
+  
+  p <- ggplot(plotdata, aes(x = log10(rank))) + 
+    geom_line(aes(y = prop_overlap, linetype = overlap_type, color = rank_coeff)) +
+    xlab("log10(marker gene rank)")
+}
+
+p <- p + 
   theme_bw() + 
-  xlab("log10(Adjusted p value limit)") + 
-  ylab("Proportion overlap significant marker genes (intersect/ union)") +
+  ylab("Proportion overlap significant marker gene") +
   facet_wrap( ~ cell_type, scales = "free_x") + 
   labs(
-    title = "Comparison of marker gene sets for pacreas cell types in Tabula Muris, pancreas context vs whole-mouse context",
-    subtitle = "At progressively more relaxed adjusted p value thresholds, note the variable x axes.",
-    caption = "Notes:\n\n1) Some cell types have few differential genes even at high p value thresholds, e.g. acinar cells\n2) Proportion of intersect tends towards 1 as the p value threshold is relaxed.\n3) Many more marker genes result from a whole mouse context than the pancreas-specific context\n4) Some cell types have non-trivial intersects across the p value range (e.g. type B pancreatic cell) implying there are context-independent marker genes, but since even at high stringency the\n    overall intersect is low (< 0.2), this set could not be identified from either marker set alone.\n5) Some cell types only develop significant intersect at low stringency (e.g. pancreatic PP cell), indicating that the marker genes are more context specific."
+    title = paste0("Comparison of marker gene sets for ", context_1 ," cell types, ", context_1, " context vs ", context_2," context")
   ) + 
   theme(plot.caption = element_text(hjust = 0))
+
+png(outfile, width = 1000, height = 800)
+print(p)
+dev.off()
+
